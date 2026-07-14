ef596ce79795668fe78f4cae471449fe056598bb
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-14 20:46:42 +0530

    five

--- five.c
+++ five.c
@@ -4,12 +4,14 @@ int main()
 
 {
     int n = 5 ;
-    for(int i = 1 ; i <= n ; i++){
-        for(int j = 1 ; j <=n-i+1;j++){
+    for(int i = n ; i >= 1 ; i--){
+        for(int j = 1 ; j<=i;j++){
+            
+
             printf("*");
         }
         printf("\n");
-        
+
     }
 return 0;
 }
---
+++ six.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+
+int main()
+
+{
+int n = 5 ;
+
+for(int i)
+return 0;
+}
