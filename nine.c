e2fdc3cbc7d17782fde1a3b0e0e203f52cd73a75
yuvrajkrishna <yuvrajkrishna26@gmail.com>
2026-07-14 20:57:14 +0530

    nine

---
+++ nine.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main()
+
+{
+int n = 5; 
+int num = 1;
+for(int i = 1 ; i <= n ; i++){
+    for(int j = 1 ; j <= i ; j++){
+        printf("%d ",num);
+        num = 1 - num;
+    }
+    printf("\n");
+}
+return 0;
+}
